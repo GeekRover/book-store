03674f2099a910333579ede97c83555db37639a2
geekrover <maherulhassan1@gmail.com>
2025-10-07 03:13:12 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <div>OKay bhul poth</div>,
+    children:[
+      {
+        index:true,
+        path: "/",
+        element: <h1>HEllo ami Home</h1>
+      },
+      {
+        path: 'about',
+        element: <h1>This is about</h1>
+      }
+    ]
+  },
+]);
